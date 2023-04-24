b069d1637aeaaaff644876090ad25f5480666964
Lobov Evgeniy <lobov.em@gmail.com>
2023-04-24 14:37:37 +0200

    New version component input

--- src/App.js
+++ src/App.js
@@ -1,66 +1,53 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, useField } from 'formik';
+import * as Yup from 'yup';
+import './App.css';
 
-function validateUsername(value) {
-  let error;
-  if (value === 'admin') {
-    error = 'Nice try!';
-  }
-  return error;
-}
+const Input = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="input" {...field} {...props} />
+      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
+    </>
+  );
+};
 
-function validateEmail(value) {
-  let error;
-  if (!value) {
-    error = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-    error = 'Invalid email address';
-  }
-  return error;
-}
-
-function validatePhone(value) {
-  let error;
-  if (!value) {
-    error = 'Required';
-  } else if (!/^(\d[-\s]?){11}\d$/i.test(value)) {
-    error = 'Invalid phone number';
-  }
-  return error;
-}
-
-export const FieldLevelValidationExample = () => (
-  <div>
-    <h1>Signup</h1>
-    <Formik
-      initialValues={{
-        username: '',
-        email: '',
-        phone: '',
-      }}
-      onSubmit={(values) => {
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      {({ errors, touched, validateForm }) => (
-        <Form>
-          <label htmlFor="username">First Name</label>
-          <Field id="username" name="username" validate={validateUsername} />
-          {errors.username && touched.username && <div>{errors.username}</div>}
-
-          <label htmlFor="email">Email</label>
-          <Field id="email" name="email" validate={validateEmail} />
-          {errors.email && touched.email && <div>{errors.email}</div>}
-
-          <label htmlFor="phone">Phone</label>
-          <Field id="phone" name="phone" validate={validatePhone} />
-          {errors.phone && touched.phone && <div>{errors.phone}</div>}
+const SignupForm = () => {
+  return (
+    <>
+      <h1 className="title">Form Formik!</h1>
+      <Formik
+        initialValues={{
+          userName: '',
+          email: '',
+          phone: '',
+        }}
+        validationSchema={Yup.object({
+          userName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+          email: Yup.string().email('Invalid email address').required('Required'),
+          phone: Yup.string()
+            .matches(/^(\d[-\s]?){11}\d$/i, 'Invalid telephone number')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form className="form">
+          <Input label="User name" name="userName" type="text" placeholder="User name" />
+          <Input label="Email Address" name="email" type="email" placeholder="Email" />
+          <Input label="Phone" name="phone" type="tel" placeholder="Phone number" />
 
           <button type="submit">Submit</button>
         </Form>
-      )}
-    </Formik>
-  </div>
-);
+      </Formik>
+    </>
+  );
+};
 
-export default FieldLevelValidationExample;
+export default SignupForm;
